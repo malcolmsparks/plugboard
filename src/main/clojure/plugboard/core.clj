28664919e08e331f62390482633fe6ea8746dccc
Malcolm Sparks <malcolm@congreve.com>
2010-08-29 18:19:21 +0100

    Added a doc-string to document one of the atoms.

--- src/main/clojure/plugboard/core.clj
+++ src/main/clojure/plugboard/core.clj
@@ -1,5 +1,8 @@
 (ns plugboard.core)
 
-(def path (atom :path))
-
-
+(def
+ ^{:doc "The relative path of the web resource. Web resources are identified
+ by URIs. Web applications are usually responsible for a number of web
+ resources, so the path given by the client must often be decoded from the
+ URI."}
+ path (atom :path))
