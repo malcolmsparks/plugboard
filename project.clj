11bd161f35a0f0989e2665d9893ba873471e16b0
Malcolm Sparks <malcolm@congreve.com>
2010-08-30 21:59:01 +0100

    Converted to use leiningen.

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject plugboard "1.0"
+  :description "A library to promote correct HTTP semantics for Clojure web applications built on Compojure"
+  :url "http://github.com/malcolmsparks/plugboard"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+
+  :dev-dependencies [[lein-clojars "0.5.0"]
+                     [swank-clojure "1.2.1"]
+                     ]
+
+  )
