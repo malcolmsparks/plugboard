48a817105674011e8b64c612e4e927d3a3f05275
Malcolm Sparks <malcolm@congreve.com>
2010-08-30 22:39:32 +0100

    Removing lein-clojars dependency, which doesn't work.

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
 
-  :dev-dependencies [[lein-clojars "0.5.0"]
+  :dev-dependencies [
                      [swank-clojure "1.2.1"]
                      ]
-
   )
