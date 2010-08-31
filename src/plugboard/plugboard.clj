db49aaf627881919013dd6c3117374c77362af6c
Malcolm Sparks <malcolm@congreve.com>
2010-08-31 22:44:50 +0100

    Changed atom to var to avoid clashes - atoms are not sufficiently unique as they cannot exist within a namespace, unlike vars.

--- src/plugboard/plugboard.clj
+++ src/plugboard/plugboard.clj
@@ -1,11 +1,13 @@
 (ns plugboard.plugboard)
 
+(def ^{:private true} _path)
+
 (def
  ^{:doc "The relative path of the web resource. Web resources are identified
  by URIs. Web applications are usually responsible for a number of web
  resources, so the path given by the client must often be decoded from the
  URI."}
- path (atom :path))
+ path (var _path))
 
 
 (def transition-map
