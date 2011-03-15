e3aa17af1bd3bc8b7dab27df5eac30679a7b2be4
Malcolm Sparks <malcolm@congreve.com>
2011-03-15 19:28:38 +0000

    Dependency version refresh.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
 ;;
 ;; Please see the LICENSE file for a copy of the GNU Affero General Public License.
 
-(defproject plugboard "1.8.2"
+(defproject plugboard "1.8.3"
   :description "A library to promote correct HTTP semantics for Clojure web applications built on Compojure"
   :url "http://github.com/malcolmsparks/plugboard"
   :dependencies [[org.clojure/clojure "1.2.0"]
@@ -23,22 +23,9 @@
 
   :dev-dependencies [
                      [swank-clojure "1.2.1"]
-                     [hiccup "0.2.6"
-                      :exclusions
-                      [org.clojure/clojure org.clojure/clojure-contrib]]
-                     [ring/ring-core "0.2.5"
-                      :exclusions
-                      [org.clojure/clojure org.clojure/clojure-contrib]]
-                     [ring/ring-jetty-adapter "0.2.5"]
-                     [compojure "0.4.1"
-                      :exclusions
-                      [org.clojure/clojure org.clojure/clojure-contrib]
-                      ]
-                     [clout "0.2.0"
-                      :exclusions
-                      [org.clojure/clojure org.clojure/clojure-contrib]
-                      ]
+                     [compojure "0.6.0"]
+                     [ring/ring-core "0.3.5"]
+                     [ring/ring-jetty-adapter "0.3.5"]
+                     [clout "0.4.0"]
                      [clj-http "0.1.1"]
-                     [autodoc "0.7.1"]
-                     ]
-  )
+                     [autodoc "0.7.1" :exclusions [org.clojure/clojure org.clojure/clojure-contrib]]])
