4a2a219f5e30f77a2f240e5445c26029ed037978
Malcolm Sparks <malcolm@congreve.com>
2011-04-21 22:27:21 +0100

    Fixed sample project.

--- sample/src/sample/main.clj
+++ sample/src/sample/main.clj
@@ -5,29 +5,29 @@
    ring.adapter.jetty
    plugboard.core.plugboard
    plugboard.webfunction.plugboards
-   sample.representations
-   ))
+   sample.representations))
 
 (ring.adapter.jetty/run-jetty
-;; plugboard.demos.main/create-application-handler
+ ;; plugboard.demos.main/create-application-handler
  (routes
-  (GET "/*" [] (plugboard.core.plugboard/merge-plugboards
+  (GET "/*" []
+       (fn [req]
+         (plugboard.webfunction.plugboards/get-response
+          req
+          (plugboard.core.plugboard/merge-plugboards
 
-   ;; We start with the defaults.
-   plugboard.core.plugboard/default-wiring
+           ;; We start with the defaults.
+           plugboard.core.plugboard/default-wiring
 
-   ;; Then add the logic that treats functions which have particular metadata
-   ;; as web resources.
-   (plugboard.webfunction.plugboards/web-function-resources
-    (map find-ns ['sample.representations]))
+           ;; Then add the logic that treats functions which have particular metadata
+           ;; as web resources.
+           (plugboard.webfunction.plugboards/web-function-resources
+            (map find-ns ['sample.representations]))
 
-   ;; Here we add a simple plugboard that adds welcome page
-   ;; behaviour when the uri ends in a slash.
-   (plugboard.webfunction.plugboards/welcome-page "index.html") 
-
-   )))
+           ;; Here we add a simple plugboard that adds welcome page
+           ;; behaviour when the uri ends in a slash.
+           (plugboard.webfunction.plugboards/welcome-page "index.html"))))))
  {:join? false
-  :port 8082}
- )
+  :port 8082})
 
 (swank.swank/start-repl)
